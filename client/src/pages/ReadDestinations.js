33076a9690f8294757bd78ccbbf9ea98a3878e8c
saipavankalyan <saipavan.munaga@gmail.com>
2023-10-27 20:19:26 -0400

    Added functionality for client side

--- client/src/pages/ReadDestinations.js
+++ client/src/pages/ReadDestinations.js
@@ -1,36 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import DestinationCard from '../components/DestinationCard';
-
+import React, { useState, useEffect } from "react";
+import DestinationCard from "../components/DestinationCard";
 
 const ReadDestinations = (props) => {
+  const [destinations, setDestinations] = useState([]);
 
-    const [destinations, setDestinations] = useState([]);
+  useEffect(() => {
+    setDestinations(props.data);
 
+    const fetchDestinations = async () => {
+      const response = await fetch("/api/destinations");
+      const data = await response.json();
+      setDestinations(data);
+    };
+    fetchDestinations();
+  }, [props]);
 
-    useEffect(() => {
-
-        setDestinations(props.data);
-    }, [props]);
-    
-
-    return (
-        <div className="ReadDestinations">
-            {
-                destinations && destinations.length > 0 ?
-                destinations.map((destination,index) => 
-                   <DestinationCard key={destination.id} 
-                         id={destination.id} 
-                         destination={destination.destination} 
-                         description={destination.description} 
-                         city={destination.city} 
-                         country={destination.country}
-                         img_url={destination.img_url}
-                         flag_img_url={destination.flag_img_url} 
-                    />
-                ) : <h3 className="noResults">{'No Destinations Yet 😞'}</h3>
-            }
-        </div>  
-    )
-}
+  return (
+    <div className="ReadDestinations">
+      {destinations && destinations.length > 0 ? (
+        destinations.map((destination, index) => (
+          <DestinationCard
+            key={destination.id}
+            id={destination.id}
+            destination={destination.destination}
+            description={destination.description}
+            city={destination.city}
+            country={destination.country}
+            img_url={destination.img_url}
+            flag_img_url={destination.flag_img_url}
+          />
+        ))
+      ) : (
+        <h3 className="noResults">{"No Destinations Yet 😞"}</h3>
+      )}
+    </div>
+  );
+};
 
 export default ReadDestinations;
